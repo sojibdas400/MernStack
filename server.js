9ce6d27b93842f0048c668509e76d2809d188419
root <root@localhost.localdomain>
2020-03-02 13:40:29 -0300

    2

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    console.log("server is running at port ${PORT}");
+    res.json({
+        message: 'welcome to our app hh' 
+    })
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log("server is running");
+    mongoose.connect('mongodb://localhost:27017/MERN', 
+        { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        },
+        ()=>{
+            console.log("database connected");
+        }
+    )
+});
+
+
+
+
+
+
+
+
